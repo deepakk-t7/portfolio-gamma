8e872a685c0b4f742dddf832ab23bdd423f640bf
deepak verma <96385390+deepakk-t7@users.noreply.github.com>
2022-07-31 19:58:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+function changeTexta() {
+  document.getElementById("a").innerHTML = "About";
+  //   document.getElementById("a").classList.add("prac");
+}
+
+function changeTextOuta() {
+  document.getElementById("a").innerHTML = "Hello.";
+  document.getElementById("a").classList.remove("prac");
+}
+
+function changeTextb() {
+  document.getElementById("b").innerHTML = "Work";
+}
+
+function changeTextOutb() {
+  document.getElementById("b").innerHTML = "I am";
+}
+
+function changeTextc() {
+  document.getElementById("c").innerHTML = "Contact";
+}
+
+function changeTextOutc() {
+  document.getElementById("c").innerHTML = "Deepak";
+}
+
+// -----------------------------
+
+function changeTextD() {
+  document.getElementById("d").innerHTML = "deepak.works";
+}
+
+function changeTextOutD() {
+  document.getElementById("d").innerHTML = "deepak";
+}
